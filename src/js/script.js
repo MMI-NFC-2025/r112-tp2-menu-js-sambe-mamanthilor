ac6fef9c166cfdac15a45695b7cea9fef04336fc
sambe-mamanthilor <maman_thilor.sambe@edu.univ-fcomte.fr>
2025-11-13 15:27:48 +0100

    classe noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,5 @@ toggle.addEventListener("click", () => {
  const isOpen = toggle.getAttribute('aria-expanded') === 'true'; 
  nav.setAttribute('aria-hidden', String(isOpen));
  toggle.setAttribute('aria-expanded', String(!isOpen));
+ document.body.classList.toggle('noscroll');
 });
